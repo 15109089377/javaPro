9844b78c388b0ec88cd8f7d903835412ed800b34
sqa <sqa@sqa-os>
2020-08-20 13:50:05 +0800

    aaaa

--- src/javaPro/hello.java
+++ src/javaPro/hello.java
@@ -4,6 +4,6 @@ public class hello {
 
 	public static void main(String[] args) {                                
 		// TODO 自动生成的方法存根
-    System.out.println("hello world");    
+    System.out.println("hello    aaaa world");    
 	}
 }
